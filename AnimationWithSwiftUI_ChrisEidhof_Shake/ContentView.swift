5408967580ac3d1507e3f65e174e754aacbf8763
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-15 22:20:44 +0200

    Initial Commit

---
+++ AnimationWithSwiftUI_ChrisEidhof_Shake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationWithSwiftUI_ChrisEidhof_Shake
+//
+//  Created by Ramill Ibragimov on 15.12.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
